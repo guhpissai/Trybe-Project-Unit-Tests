3cd5013efb9d2eb22c25ddaad47d17ddd1ddc75a
guhpissai <gustavopissai3d@gmail.com>
2022-11-23 15:33:26 -0300

    Requisito 1 concluido

--- src/average.js
+++ src/average.js
@@ -11,7 +11,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  let result;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof(array[index]) != 'number') {
+      return undefined;
+    } 
+    sum += array[index];
+    result = sum / array.length;
+  }
+  return Math.round(result);
+};
+console.log(average([]));
 
 module.exports = average;
 
